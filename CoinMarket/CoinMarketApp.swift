579e6a58d9c0a19b65b9d2b986c7f4ac0f29fa0b
Phuc Hoang <phuchoang022002@gmail.com>
2023-09-08 13:16:23 +0700

    Initial Commit

---
+++ CoinMarket/CoinMarketApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinMarketApp.swift
+//  CoinMarket
+//
+//  Created by Phuc Hoang on 08/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct CoinMarketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
