ffd1c8730337360423de913422c3723657da569b
AnhNG <77968170+nathan-nas@users.noreply.github.com>
2023-09-22 18:18:23 +0700

    image picker

--- CoinMarket/ViewModel/BuyViewModel.swift
+++ CoinMarket/ViewModel/BuyViewModel.swift
@@ -9,13 +9,6 @@ import Foundation
 import Firebase
 import FirebaseAuth
 
-struct UserInfo: Identifiable, Codable {
-    var id: String
-    var name: String
-    var balance: String
-}
-
-
 class BuyViewModel: ObservableObject {
     private var auth = AuthViewModel()
     let db = Firestore.firestore()
